c6b1ceb47d6d9e43253eaaac4287c60da057621f
Timi Ombe <49966180+Timmiblakes@users.noreply.github.com>
2021-02-07 11:04:48 +0100

    Update Contact saver.py

--- Contact saver.py
+++ Contact saver.py
@@ -1,21 +1,25 @@
 # Add, Update, Delete and List contacts
-# The implementation of the database written files on a notepad file is the only implementation remaining
 
 def main():
-    # Remove this dummy data and use the database (notepad) very soon.
-    original_database_for_names = ["EMMA", "JENNIFER", "JOSH", "FRIDAY"]
-    original_database_for_numbers = [8054564778, 7058764779, 9064789112, 5012345778]
+    # Creating a list and getting the data from a .txt file in the python folder.
+    database_names = []
+    database_numbers = []
+    original_database_for_names, original_database_for_numbers = get_info_from_database(database_names, database_numbers
+                                                                                        )
 
-    # Forking the database
+    # Forking the database to a temporary list so the original list doesn't get messed up during the process of editing
     list_of_numbers = original_database_for_numbers.copy()
     list_of_names = original_database_for_names.copy()
 
-    # Getting user input
+    # Listing the contacts
+    listing(list_of_names, list_of_numbers)
+
+    # Getting user input for what action to perform
     menu_feedback = input("Do you want to add, delete, update or list the numbers?\nEnter 'A' to Add a contact\n"
                           "Enter 'D' to Delete a contact\nEnter 'U' to update a contact information\n"
                           "Enter 'L' to list your contacts\n\nEnter your reply here: ")
 
-    # Checking for what task the user wants to perform
+    # Checking for what action the user wants to perform
     if (menu_feedback == 'A') or (menu_feedback == 'a'):
         adding(list_of_names, list_of_numbers)
     elif (menu_feedback == 'D') or (menu_feedback == 'd'):
@@ -25,64 +29,90 @@ def main():
     elif (menu_feedback == 'L') or (menu_feedback == 'l'):
         listing(list_of_names, list_of_numbers)
     else:
-        print("Your input is invalid")
+        print("\nYour input is invalid\n")
 
 
 def database(list_of_names, list_of_numbers):
-    # This function merges that forked data with the actual database
-    original_database_for_names = list_of_names
-    original_database_for_numbers = list_of_numbers
-    original_database_for_names.upper()
+    # Takes the edited list that was copied earlier on, clears the .txt file, and then replaces it with the edited one
+    with open('Name Database.txt', 'w') as names:
+        names.truncate(0)
+        for item in list_of_names:
+            names.write("%s\n" % item)
+    with open('Phone Number Database.txt', 'w') as numbers:
+        numbers.truncate(0)
+        for item in list_of_numbers:
+            numbers.write("%s\n" % item)
+
+
+def get_info_from_database(database_for_names, database_for_numbers):
+    # This function is called upon to get the names and numbers from two .txt files (Name Database,
+    # Phone Number Database) and then add it to a list, and then the list is returned.
+    with open('Name Database.txt', 'r') as names:
+        for line in names:
+            last_character = line[:-1]
+            database_for_names.append(last_character)
+    with open('Phone Number Database.txt', 'r') as numbers:
+        for a in numbers:
+            last_character_line = a[:-1]
+            database_for_numbers.append(last_character_line)
+    return database_for_names, database_for_numbers
 
 
 def adding(list_of_names, list_of_numbers):
-    # The adding operation
+    # This function gets the name and number to be added and then sends it to the database function
     contact_name = input("Enter the name of the contact you would like to store: ")
     contact_number = input("Enter the phone number of the contact you would like to store: ")
-    all_caps_names = contact_name.upper()
-    list_of_names.append(all_caps_names)
+    list_of_names.append(contact_name)
     current_number = int(contact_number)
     list_of_numbers.append(current_number)
     database(list_of_names, list_of_numbers)
 
 
 def deleting(list_of_names, list_of_numbers):
-    # The deleting operation
-    name = input("Enter the name of the person you would like to delete from your contact list: ")
-    contact_name = name.upper()
-    if contact_name in list_of_names:
-        position = list_of_names.index(contact_name)
-        list_of_names.remove(list_of_names[position])
-        list_of_numbers.remove(list_of_numbers[position])
-        database(list_of_names, list_of_numbers)
-    else:
-        print("Invalid name")
+    # This deletes contacts based on the name provided; if not available then it asks for input again
+    signal = False
+    while signal is False:
+        contact_name = input("Pleas enter the full name of the person you would like to delete from your contact list: "
+                             )
+        temp_name = contact_name.upper()
+        for elements in list_of_names:
+            placeholder = elements.upper()
+            if temp_name == placeholder:
+                position = list_of_names.index(elements)
+                list_of_names.remove(list_of_names[position])
+                list_of_numbers.remove(list_of_numbers[position])
+                signal = True
+            else:
+                pass
+    database(list_of_names, list_of_numbers)
 
 
 def updating(list_of_names, list_of_numbers):
-    # The updating operation
-    # Asking whether the user wants to update the name or phone number of the contact or both.
+    # This function updates the names of users in the forked list and then sends it to the database
+    # It can change just the name or phone number at a time
     signal = False
     while signal is False:
         response = input("What would you like to update -  the name or the number of the contact?\nEnter in 'N'"
                          "for name or 'P' for phone number: ")
         if (response == 'N') or (response == 'n'):
-            contact_name = input("Enter the name of the contact you would like to update: ")
-            upper_contact_name = contact_name.upper()
-            if upper_contact_name in list_of_names:
-                position = list_of_names.index(upper_contact_name)
-                name = input("Please enter the replacement name: ")
-                updated_name = name.upper()
-                list_of_names.remove(list_of_names[position])
-                list_of_names.insert(position, updated_name)
-                signal = True
-            else:
-                print("Name not found!!")
+            contact_name = input("Enter the full name of the contact you would like to update: ")
+            temp_name = contact_name.upper()
+            for elements in list_of_names:
+                placeholder = elements.upper()
+                if temp_name == placeholder:
+                    position = list_of_names.index(elements)
+                    name = input("Please enter the replacement name in full: ")
+                    list_of_names.remove(list_of_names[position])
+                    list_of_names.insert(position, name)
+                    signal = True
+                else:
+                    pass
+            print("\nThe name given is not part of our database!!\n")
+
         elif (response == 'p') or (response == 'P'):
             contact_number = input("Enter the number of the contact you would like to update: ")
-            int_contact_number = int(contact_number)
-            if int_contact_number in list_of_numbers:
-                position = list_of_numbers.index(int_contact_number)
+            if contact_number in list_of_numbers:
+                position = list_of_numbers.index(contact_number)
                 updated_number = input("Please enter the replacement number: ")
                 list_of_numbers.remove(list_of_numbers[position])
                 list_of_numbers.insert(position, updated_number)
@@ -90,12 +120,12 @@ def updating(list_of_names, list_of_numbers):
             else:
                 print("Number not found!!")
         else:
-            print("Invalid input!!")
+            print("\nInvalid input!!\n")
     database(list_of_names, list_of_numbers)
 
 
 def listing(list_of_names, list_of_numbers):
-    # The listing operation
+    # This operation just lists the names and accompanying currently in the .txt file
     length_of_database = len(list_of_names)
     for i in range(0, length_of_database):
         print(list_of_names[i], "\n", list_of_numbers[i], "\n", sep="")
